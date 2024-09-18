0f5d24698a6279f88280f768f39726cffddc36e8
erikantonyian <erikantonian@gmail.com>
2024-09-19 01:25:29 +0400

    updating help.txt and adding more information aboud deployment

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+import os
 
 app = Flask(__name__)
 
+SERVICE_NAME = os.environ.get("SERVICE_NAME", "application")
 
-@app.route("/hello/<username>")
-def say_hello(username: str):
-    return f"Hi, {username}!"
+
+@app.route("/hello/<user>")
+def say_hello(user: str):
+    return make_response(jsonify({"message": f"Hi from {SERVICE_NAME}, {user}!"}), 200)
 
 
 if __name__ == "__main__":
